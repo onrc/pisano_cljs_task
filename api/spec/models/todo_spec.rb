a0c9e6a1ba15e91d2cd097f7c201385b017e34d2
onur <mail@onrc.pw>
2020-06-05 22:10:10 +0300

    add specs for models

--- api/spec/models/todo_spec.rb
+++ api/spec/models/todo_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:list) }
+  it { should validate_presence_of(:title) }
 end
