525fbd5817236c75b1e45e53bdf7a5da6c4b06d3
onur <mail@onrc.pw>
2020-06-08 05:56:15 +0300

    remove unnecessary line from list_spec

--- api/spec/models/list_spec.rb
+++ api/spec/models/list_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   it { should have_many(:todos).dependent(:destroy) }
   it { should validate_presence_of(:title) }
 end
