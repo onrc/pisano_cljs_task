c50d08aed161c16bfccede5c3e34ad97c2e35db7
onur <mail@onrc.pw>
2020-06-09 22:15:52 +0300

    remove unnecessary lines

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def render_unprocessable_entity_response(error)
-    render json: error.record.errors, status: :unprocessable_entity
-  end
-
   def render_not_found_response(error)
     render json: { error: error.message }, status: :not_found
   end
