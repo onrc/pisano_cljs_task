ed544e75c333b2aa855da04d4db5f211938dd9ba
onur <mail@onrc.pw>
2020-06-09 22:16:20 +0300

    update todos routing spec

--- api/spec/routing/todos_routing_spec.rb
+++ api/spec/routing/todos_routing_spec.rb
@@ -3,28 +3,28 @@ require "rails_helper"
 RSpec.describe TodosController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/todos").to route_to("todos#index")
+      expect(get: "/lists/1/todos").to route_to("todos#index", list_id: "1")
     end
 
     it "routes to #show" do
-      expect(get: "/todos/1").to route_to("todos#show", id: "1")
+      expect(get: "/lists/1/todos/1").to route_to("todos#show", list_id: "1", id: "1")
     end
 
 
     it "routes to #create" do
-      expect(post: "/todos").to route_to("todos#create")
+      expect(post: "/lists/1/todos").to route_to("todos#create", list_id: "1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/todos/1").to route_to("todos#update", id: "1")
+      expect(put: "/lists/1/todos/1").to route_to("todos#update", list_id: "1", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/todos/1").to route_to("todos#update", id: "1")
+      expect(patch: "/lists/1/todos/1").to route_to("todos#update", list_id: "1", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/todos/1").to route_to("todos#destroy", id: "1")
+      expect(delete: "/lists/1/todos/1").to route_to("todos#destroy", list_id: "1", id: "1")
     end
   end
 end
